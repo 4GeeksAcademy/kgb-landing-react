5cc2f5228b4fb2deb7ce6d4221f260cee4bfc73b
KGBeneby <167141463+KGBeneby@users.noreply.github.com>
2024-08-26 20:05:31 +0000

    layout set up and componets

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+            <div className="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
